396eaa2d0939abc101d2cac1098b2b020db8a815
SuSsaHin <SuSsaHin@ya.ru>
2014-11-23 21:22:44 +0500

    Initial commit

---
+++ ConsoleCalculator/Calculator.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleCalculator
+{
+	public class Calculator
+	{
+		private enum State
+		{
+			Initial,
+			Integer,
+			Decimal
+		}
+
+		private static State currentState;
+		private static string currentNumber;
+
+		public static double Calculate(string input)
+		{
+			currentNumber = "";
+			currentState = State.Initial;
+
+			foreach (var c in input)
+			{
+				switch (currentState)
+				{
+					case State.Initial:
+						ProcessInitial(c);
+						break;
+					case State.Integer:
+						ProcessInteger(c);
+						break;
+					case State.Decimal:
+						ProcessDecimal(c);
+						break;
+				}
+			}
+
+			return Double.Parse(currentNumber, new CultureInfo("en-US"));
+		}
+
+		private static void ProcessInitial(char c)
+		{
+			if (Char.IsDigit(c))
+			{
+				currentState = State.Integer;
+				ProcessInteger(c);
+			}
+			else 
+			{
+				throw new Exception("Unexpected char in input: " + c);
+			}
+		}
+
+		private static void ProcessInteger(char c)
+		{
+			if (Char.IsDigit(c))
+			{
+				currentNumber += c;
+			}
+			else if (c == '.')
+			{
+				currentNumber += c;
+				currentState = State.Decimal;
+			}
+			else
+			{
+				throw new Exception("Unexpected char in input: " + c);
+			}
+		}
+
+		private static void ProcessDecimal(char c)
+		{
+			if (Char.IsDigit(c))
+			{
+				currentNumber += c;
+			}
+			else
+			{
+				throw new Exception("Unexpected char in input: " + c);
+			}
+		}
+
+	}
+}
---
+++ TestCalculator/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using ConsoleCalculator;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace TestCalculator
+{
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestCase("55")]
+		[TestCase("10")]
+		[TestCase("1.0")]
+		public void TestParsing(string input)
+		{
+			Calculator.Calculate(input);
+		}
+	}
+}
